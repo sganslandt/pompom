d9ed5e85ced4a9a4a555fad9d2f2f4cc3232b84e
Victor Wihlborg <victor@baquero-wihlborg.se>
2013-06-19 12:53:58 +0200

    Update to jquery 2.0.2 and add map

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 requirejs.config({
     "baseUrl": "assets/js",
     "paths": {
-        "jquery": "vendor/jquery-2.0.0.min",
+        "jquery": "vendor/jquery-2.0.2.min",
         "modernizr": "vendor/modernizr-2.6.2.min",
         "sortable": "vendor/jquery-ui-1.10.3.custom.min",
         "touch-punch": "vendor/jquery.ui.touch-punch.min"
